a317f6a2a996a56ce009a489e56f65689c2d7038
Nathan Bedford <nathan.bedford@gmail.com>
2017-12-10 23:29:53 -0500

    Got skeleton working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // create a new component. Should produce some HTML
-const App = function() {
+const App = () => {
     return <div>Hi!</div>;
 }
 
 // take this component's generated html and put in on the page
+ReactDOM.render(<App/>, document.querySelector('.container'));
