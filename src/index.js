d56233a4af7048dc3a636fd025dcaae2d2884de2
Nathan Bedford <nathan.bedford@spectrumhealth.org>
2017-12-13 19:07:39 -0500

    Got to custom seaches

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyBkC76gu2uQPtPSn7z9wzkrBqRc25ddMDs';
 // create a new component. Should produce some HTML
-const App = () => {
-    return <div>Hi!</div>;
+class App extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({key:API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render () {
+        return (
+            <div>
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+                <VideoDetail video={this.state.selectedVideo }/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}/>
+            </div>
+        );
+    }
 }
 
 // take this component's generated html and put in on the page
