1c8ae51bbd3e6f19739eee6e98c2ad7b706df4f1
Nathan Bedford <nathan.bedford@spectrumhealth.org>
2017-12-13 19:39:34 -0500

    Moving on to Redux

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,17 +10,26 @@ class SearchBar extends Component{
 
     render() { 
         return (
-            <div className="search-bar">
-                <input 
-                    value={this.state.term}
-                    onChange={ event => this.onInputChange(event.target.value)}/>
-            </div>
+            <form onSubmit={ (e) => {e.preventDefault(); this.onSearchClick();}}>
+                <div className="search-bar">
+                    <input 
+                        value={this.state.term}
+                        onChange={ event => this.onInputChange(event.target.value)}/>
+                        <button 
+                            type="submit"
+                            className="btn btn-primary">Search</button>
+                </div>
+            </form>
         );
     }
 
     onInputChange(term){
         this.setState({term});
-        this.props.onSearchTermChange(term);
+        //this.props.onSearchTermChange(term);
+    }
+
+    onSearchClick(){
+        this.props.onSearchTermChange(this.state.term);
     }
 }
 
